df76da51f45ccb63606dec75566dd2ce68501273
thinny <tony.sutin1234@gmail.com>
2018-02-26 02:54:44 +0700

    add ionic3 video capture and upload to storage
    add ionic3 video capture and upload to storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import * as firebase from 'firebase';
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBXh1LjxE22AwPj2zgMp1jip_Q1U0MkKgk",
+    authDomain: "ion3-7bb36.firebaseapp.com",
+    databaseURL: "https://ion3-7bb36.firebaseio.com",
+    projectId: "ion3-7bb36",
+    storageBucket: "ion3-7bb36.appspot.com",
+    messagingSenderId: "1033574228295"
+  }
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +26,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
+import { File } from '@ionic-native/file';
+import { FilePath } from '@ionic-native/file-path';
+import * as firebase from 'firebase';
+import { AngularFireDatabase } from 'angularfire2/database';
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [MediaCapture, File, FilePath, AngularFireDatabase]
 })
 export class HomePage {
+  videoObject: any;
+  myBlob: any;
+  constructor(public navCtrl: NavController,
+              private mediaCap: MediaCapture,
+              private file: File, private filePath: FilePath,
+              private db: AngularFireDatabase) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  captureVideo(){
+      this.mediaCap.captureVideo({limit: 1, duration: 60, quality: 1}).then((data: MediaFile[]) => {;
+        let index = data[0].fullPath.lastIndexOf('/'), finalPath = data[0].fullPath.substr(0, index);
+        this.file.readAsArrayBuffer(finalPath, data[0].name).then((file) => {
+          let blob = new Blob([file], {type: data[0].type});
+          this.myBlob = blob;
+          this.uploadVideoToStorage();
+        });
+      });
+    }
+  uploadVideoToStorage(){
+    let storageRef =  firebase.storage().ref();
+    let task = storageRef.child('videos/'+ new Date().getTime()+".mp4").put(this.myBlob);
+    task.on('state_changed', (snapshot: any) => {
+      let uploading = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+      console.log("upload is " + uploading + "% done");
+      if(uploading == 100){
+        alert('uploaded OK');
+      }
+    });
   }
 
 }
